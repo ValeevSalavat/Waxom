203a7a58357591d2b59b7b5b1f06d8bdd8592154
ValeevSalavat <salik95@mail.ru>
2021-03-18 23:08:42 +0400

    1дз. Динамическое формирование каталога.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-
+//Video start code block
+/*
 var video = document.querySelector("#video"),
 button = document.querySelector(".video-presentation__btn-play");
 
@@ -6,4 +7,40 @@ button.addEventListener("click", function() {
     video.play()
     video.setAttribute("controls","controls");
 }, false);
+*/
 
+const products = [
+    { title: 'Shirt', price: 150 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+  ];
+
+  //{title,price} - используется деструктуризация
+  const renderGoodsItem = ({title,price}) => {
+    return `<div class="catalog__list-item"><h1>${title}</h1><p>${price}</p></div>`;
+  };
+  
+  const renderGoodsList = (list,querySelector) => {
+     let toRender = list.map(item=>renderGoodsItem(item)).join('');
+     console.log(toRender);
+     document.querySelector(querySelector).innerHTML = toRender;
+  }
+
+  renderGoodsList(products,'.catalog__list>.content');
